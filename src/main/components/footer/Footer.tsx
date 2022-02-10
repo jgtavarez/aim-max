04ff6001ba7309835a7231e6c6c9ee5483748742
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2022-02-10 01:12:24 -0400

    style: footer

--- src/main/components/footer/Footer.tsx
+++ src/main/components/footer/Footer.tsx
@@ -2,18 +2,18 @@ import { FooterProps } from '../../interfaces/main.interfaces';
 
 export const Footer = ({ children }: FooterProps) => {
     return (
-        <div className='bg-bold-blue text-medium-gray'>
-            <footer className='flex flex-col items-center container mx-auto'>
-                <h5>© 2021 Juan López</h5>
-                <div>
+        <div className='bg-bold-blue text-medium-gray py-6'>
+            <footer className='flex flex-col items-center container mx-auto gap-5'>
+                <h5 className='hidden'>© 2021 Juan López</h5>
+                <div className='flex flex-col justify-center items-center gap-5'>
                     <img src="./logo-small.png" alt="logo" />
-                    <div className="hidden">
+                    <div className="text-center cursor-pointer">
                         <h5>Terms of Service</h5>
                         <h5>Privacy Policy</h5>
                         <h5>Security</h5>
                         <h5>Sitemap</h5>
                     </div>
-                    <div>
+                    <div className='flex items-center gap-5'>
                         {children}
                     </div>
                 </div>
